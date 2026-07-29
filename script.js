f5a91a2af7a7825dabc1626df58400043c134ff5
saalim-tech <saalimpathan24@gmail.com>
2026-07-29 23:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,40 @@ Your registration has been received successfully.`
     form.reset();
 
 });
+const reading = document.getElementById("reading");
+const sports = document.getElementById("sports");
+const music = document.getElementById("music");
+const coding = document.getElementById("coding");
+
+const readingFields = document.getElementById("readingFields");
+const sportsFields = document.getElementById("sportsFields");
+const musicFields = document.getElementById("musicFields");
+const codingFields = document.getElementById("codingFields");
+
+reading.addEventListener("change", function () {
+
+    readingFields.style.display =
+        reading.checked ? "block" : "none";
+
+});
+
+sports.addEventListener("change", function () {
+
+    sportsFields.style.display =
+        sports.checked ? "block" : "none";
+
+});
+
+music.addEventListener("change", function () {
+
+    musicFields.style.display =
+        music.checked ? "block" : "none";
+
+});
+
+coding.addEventListener("change", function () {
+
+    codingFields.style.display =
+        coding.checked ? "block" : "none";
+
+});
